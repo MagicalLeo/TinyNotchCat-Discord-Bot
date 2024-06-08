8657e3fc8d2041f35ba1d32c88ea33e1c9264b69
ajimmyyy <80930376+ajimmyyy@users.noreply.github.com>
2024-06-08 22:18:40 +0800

    debug-ignore

--- cogs/drawStraw.py
+++ cogs/drawStraw.py
@@ -43,10 +43,5 @@ class DrawStraw(commands.Cog):
         responses = ["是", "否", "可能", "不確定", "絕對"]
         await interaction.response.send_message(random.choice(responses))
 
-    @app_commands.command(name = "complain", description = "抱怨箱online")
-    @app_commands.describe(a = "輸入數字", b = "輸入數字")
-    async def complain(self, interaction: discord.Interaction, a: int, b: int):
-        await interaction.response.send_message(f"結果: {a+b}")
-
 async def setup(bot: commands.Bot):
     await bot.add_cog(DrawStraw(bot))
